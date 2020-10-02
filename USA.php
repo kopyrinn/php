c9cf83091bf07c56349c9a4582df27b573801139
kopyrinn <51287160+kopyrinn@users.noreply.github.com>
2020-10-02 03:37:23 +0300

    Add files via upload

---
+++ USA.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+
+<?php 
+	echo $_GET["where"];
+	echo $_GET["from"];
+	echo $_GET["text"];
+?>
+
+
+<form action="index.php" method="GET">
+	<input type="" name="where">
+	<input type="" name="from">
+	<input type="" name="text">	
+	<button>отправить</button>	
+</form>
---
+++ index.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+
+<h1>HELLO</h1>
+
+<form action="USA.php" method="GET">
+	<input type="" name="where"> <!--От кого-->
+	<input type="" name="from"> <!--Кому-->
+	<textarea></textarea>
+	<button>Отправить</button>
+</form>
+
+
+
+<?php
+	echo $_GET["where"];
+?>
+
+
+
+
+
+
+
+<?php
+	echo $_GET["from"];
+?>
