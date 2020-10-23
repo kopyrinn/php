ed822d9ef74632a9135d07ea7b61a33131453331
kopyrinn <51287160+kopyrinn@users.noreply.github.com>
2020-10-23 11:20:10 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
 	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
+
+	<?php 
+		echo "<h1 style='color: red'> Hello world! </h1>";
+	?>
+
+
 	
 
-
-	<form action="usa.php" method="GET">
-		<input type="" name="age">
-		<button>Отправить</button>
-	</form>
 
 
 </body>
