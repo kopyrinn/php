378248f882756b674be477283601bfafc9a27e97
kopyrinn <51287160+kopyrinn@users.noreply.github.com>
2020-10-23 10:53:06 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,88 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-</head>
-<body>
+<meta charset="utf-8">
 
-	<?php
-	
-		$text = "<h1>Vasya</h1>";
-		$div = "<div style='' class=''></div>";
-			
-	?>
+<h1>HELLO</h1>
 
-	<?php
-		echo "Hello!";
-		echo $div;
-		echo $text;
-	?>	
-	<?php
-		$array = ["kolya","vasya","harry"];		//массив
-		echo $array[0];
-		echo $array[2];
-
-		for ($i=0; $i<3 ; $i=$i+1) {  //цикл
-			echo $array[$i];
-		}
-
-	?>
-	<h1>
-		<?php
-			echo $array[0];
-		?>		
-	</h1>
-	<?php
-
-	?>
+<form action="USA.php" method="GET">
+	<input type="" name="where"> <!--От кого-->
+	<input type="" name="from"> <!--Кому-->
+	<textarea></textarea>
+	<button>Отправить</button>
+</form>
 
 
-	<?php 
-		$val = [ //ассоциативный массив ака объект
-			"name" => "Nick",
-			"age" => 14,
-			"hobby" => "swim"
-		];
 
-		for ($i=0; $i < 3; $i++) { 
-			$val["name"];
-			object.name
-		}
-
-		$arrayVal = [ //ассоц массив внутри массива
-			[
-				"name" => "Nick",
-				"age" => 14,
-				"hobby" => "swim"
-			],
-			[
-				"name" => "Vova",
-				"age" => 15,
-				"hobby" => "prog"
-			],
-
-		]
-		for ($i=0; $i < 2 ; $i++) { 
-			echo $arrayVal[$i]["name"];	
-		}
-		
-	?>
-
-	<h1>
-		<?php
-			echo $val["name"];
-
-			
-		?>
-	</h1>
+<?php
+	echo $_GET["where"];
+?>
 
 
-		<?php
-			$img = ["img/2.png", "img/3.png"]
 
-			echo "<img src='" + $img[0] + "'>";
-		?>
 
-</body>
-</html>
+
+
+
+<?php
+	echo $_GET["from"];
+?>
